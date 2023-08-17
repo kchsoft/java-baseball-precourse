7e4076bec7e2a553d0ca372c11b9c06fb70d23c2
kchranger <kchranger.ajou.ac.kr>
2023-08-17 15:30:34 +0900

    feat : compare user input with TargetNumber

--- src/main/java/baseball/Model/TargetNum.java
+++ src/main/java/baseball/Model/TargetNum.java
@@ -15,4 +15,52 @@ public class TargetNum {
         BSN = "000"; // init
     }
 
+    public boolean calResult(int num) {
+        int[] bs = {0, 0}; // Ball and Strike
+        int[] guessArr = divideNum(num);
+
+        compareNum(bs, guessArr[0], 1);
+        compareNum(bs, guessArr[1], 2);
+        compareNum(bs, guessArr[2], 3);
+        setBSN(bs);
+
+        if(bs[1] == 3) return true;
+        return false;
+    }
+
+    private int[] divideNum(int num) {
+        int[] arr = new int[3];
+
+        arr[0] = num / 100;
+        num %= 100;
+
+        arr[1] = num / 10;
+        num %= 10;
+
+        arr[2] = num;
+        return arr;
+    }
+
+    private void compareNum(int[] bs, int guessNum, int index) {
+        if (target.containsKey(guessNum)) {
+            if (target.get(guessNum) == index)
+                bs[1] += 1;
+            else
+                bs[0] += 1;
+        }
+        return;
+    }
+
+    public String getBSN() {
+        return BSN;
+    }
+
+    private void setBSN(int[] bs) {
+        if (bs[0] == 0 && bs[1] == 0) {
+            BSN = "001"; // Nothing
+            return;
+        }
+        BSN = "" + bs[0] + bs[1] + "0";
+        return;
+    }
 }
