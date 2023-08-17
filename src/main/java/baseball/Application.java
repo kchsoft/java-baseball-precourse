cbeb01aacdd276d3af48ef786adb55ea597bb7f0
kchranger <kchranger.ajou.ac.kr>
2023-08-17 12:21:53 +0900

    feat : user input , game guide output , restart game

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,21 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
     public static void main(String[] args) {
-        //TODO: 숫자 야구 게임 구현
+        int gameProcess = 1;
+
+        while(gameProcess == 1){
+            System.out.printf("숫자를 입력해 주세요. : ");
+            String tmp =  Console.readLine(); // tmp -> user input
+
+            if(true) { // if user input answer
+                System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+                System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+                gameProcess = Integer.parseInt(Console.readLine());
+            }
+        }
+
     }
 }
---
+++ src/main/java/baseball/View/OutPutView.java
@@ -0,0 +1,12 @@
+package baseball.View;
+import camp.nextstep.edu.missionutils.Console;
+
+public class OutPutView {
+    public OutPutView() {}
+
+    public static void printInputNumber() {
+    }
+
+
+}
+
